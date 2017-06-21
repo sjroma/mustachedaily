2fd60ec14515de7070675d3c694a37ef5f739c27
Steve Roma <sjroma@hotmail.com>
2017-06-21 10:09:40 -0400

    additional comments for future me

--- express-mustache.js
+++ express-mustache.js
@@ -11,8 +11,12 @@ app.set('view engine', 'mustache')
 
 // TODO put your routes here
 app.get('/', function (req, res) {
-  res.render('index', {users: data.users});
+  res.render('index', {users: data.users}); // gather user data for the index page
 });
+
+//app.get('/:dynamic_route', function (req, res) { // path to get all individual user info
+//  res.send(res.params.dynamic_route);
+//});
 
 app.listen(3000, function () {
   console.log('Successfully started express-mustache application!');

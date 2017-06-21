ab2dd1520053a11220bfd6b63d1a82fa2bf10730
Steve Roma <sjroma@hotmail.com>
2017-06-21 09:16:37 -0400

    add comments for future me

--- express-mustache.js
+++ express-mustache.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 
-const data = require("./data");
+const data = require("./data"); // looks for data in the dir...don't have to specify data.js
 const app = express();
 app.use(express.static('public'));
 
-app.engine('mustache', mustacheExpress());
+app.engine('mustache', mustacheExpress());  // templating engine
 app.set('views', './views')
 app.set('view engine', 'mustache')
 
